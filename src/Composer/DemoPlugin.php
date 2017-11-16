5f3780530fdd15d8ea6162b940b04e2d38a79a7a
Frederic G. MARAND <fgm@osinet.fr>
2017-11-16 14:07:57 +0100

    Show all events with flags and params.

--- src/Composer/DemoPlugin.php
+++ src/Composer/DemoPlugin.php
@@ -3,17 +3,59 @@
 namespace Fgm\Composer;
 
 use Composer\Composer;
+use Composer\EventDispatcher\Event;
 use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
+use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\ScriptEvents;
 
 class DemoPlugin implements EventSubscriberInterface, PluginInterface {
+  const NAME = 'DemoPlugin';
+
   public function activate(Composer $composer, IOInterface $io) {
 
   }
 
   public static function getSubscribedEvents() {
-    return [];
+    $rc = new \ReflectionClass(ScriptEvents::class);
+    $names = $rc->getConstants();
+    $ret = [];
+    foreach ($names as $name) {
+      $res[$name] = ['onEvent'];
+    }
+
+    $rc = new \ReflectionClass(PackageEvents::class);
+    $names = $rc->getConstants();
+    foreach ($names as $name) {
+      $res[$name] = ['onEvent'];
+    }
+
+    $rc = new \ReflectionClass(PluginEvents::class);
+    $names = $rc->getConstants();
+    foreach ($names as $name) {
+      $res[$name] = ['onEvent'];
+    }
+
+    return $res;
+  }
+
+  protected function show($v) {
+    if (is_array($v) && !empty($v)) {
+      echo self::NAME . ': ' . json_encode($v) . "\n";
+    }
+    elseif (is_scalar($v)) {
+      echo self::NAME . ": $v\n";
+    }
+  }
+
+  public function onEvent($event) {
+    $this->show($event->getName());
+    $this->show(get_class($event));
+    $this->show($event->getFlags());
+    $this->show($event->getArguments());
+    echo str_repeat('-', 80) . "\n";
   }
 
 }
